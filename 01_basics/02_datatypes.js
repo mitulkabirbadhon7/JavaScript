e2c6eaf052a670345099f96072263ad71a4f3e9e
MD Mitul Kabir Badhon <126774549+mitulkabirbadhon7@users.noreply.github.com>
2026-02-03 17:03:06 +0000

    datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as newer version
+
+console.log(3+3)
+
+console.log("Badhon");
+
+let name = "Badhon"
+let age = 23
+let isApproved = false
+let state
+let address = null
+
+console.table([name,age,isApproved,state,address])
+
+// number => 2 to power 53
+// bigInt => larger than 2 to power 53
+// string =>  ""
+//boolean => true or false
+// undefined => value not assigned
+// null => standalone value
+// symbol => unique identifier
+
+// object => key value pair
+
+console.log("type of name is =",typeof name) // this is how we check datatype 
+/*
+ data type of null is = object 
+ data type of undefined is = undefined
+
+ important for interview questions
+*/
+
