4beb4adb882fdd616f0b47d20ee8a7a9aba6452a
MD Mitul Kabir Badhon <126774549+mitulkabirbadhon7@users.noreply.github.com>
2026-02-03 15:58:57 +0000

    variable_in_javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 123456;
+let accountGmail = "mkb@gmail.com"
+var accountPassword = "mkb@1234"
+accoutCity = "New York" 
+
+//change the value of variables
+
+accountGmail = "badhon1245@gmail.com"
+accountPassword= "212121"
+accoutCity = "Los Angeles"
+
+/*
+
+prefer to not use var because of it makes issue in block scope
+
+*/
+
+
+//accountId = 2
+//console.log(accountId);
+//console.log(accountGmail);
+//console.log(accountPassword);
+//console.log(accoutCity);
+console.table([accountId, accountGmail, accountPassword, accoutCity]);
