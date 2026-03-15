d70c5e728017124493aba26f6a66402ae18b92be
MD Mitul Kabir Badhon <126774549+mitulkabirbadhon7@users.noreply.github.com>
2026-03-15 20:29:20 +0000

    basicsoperation

---
+++ 01_basics/operations.js
@@ -0,0 +1,30 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2 + 3) // addition
+// console.log(5 - 2) // subtraction
+// console.log(4 * 2) // multiplication
+// console.log(10 / 2) // division
+// console.log(10 % 3) // modulus
+
+// let str1 = "Hello"
+// let str2 = "World"
+// let str3 = str1 + " " + str2
+// console.log(str3) 
+
+// console.log(1+"2"); // 12
+// console.log("1"+2+2); //122 
+// console.log(1+2+"2"); // 32 
+// console.log("1"+(2+2)); // 14 
+
+console.log(+ true) //1
+
+let num1,num2 , num3
+num1 = num2 = num3 = 2 +2 
+console.log(num1,num2,num3)
+
+let count = 0
+count++
+console.log(count) // 1
+
