67f07ab6c51ef27450c2cd4c4ff030e11cb31427
MD Mitul Kabir Badhon <126774549+mitulkabirbadhon7@users.noreply.github.com>
2026-03-15 22:06:58 +0000

    comparison

---
+++ 01_basics/Comparison.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1) 
+// console.log(2 < 1) 
+// console.log(2 >= 2) 
+// console.log(2 <= 1) 
+// console.log(2 == 2) 
+// console.log(2 != 3) 
+
+console.log("2">1);
+console.log("02">1);
+// === strict equality operator means it checks both value and type 
+console.log("2" === "2");
+console.log("2" === 2); 
+
+
+
