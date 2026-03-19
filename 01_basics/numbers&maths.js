2630c07b87f23d24509d5ab62f9957558291e931
MD Mitul Kabir Badhon <126774549+mitulkabirbadhon7@users.noreply.github.com>
2026-03-19 21:57:09 +0000

    numbers and math update

---
+++ 01_basics/numbers&maths.js
@@ -0,0 +1,40 @@
+const score = 100
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(typeof balance.toString());
+// console.log(balance.toFixed(2));
+
+const num1 = 123.8966
+
+
+// console.log(num1.toPrecision(4));
+
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-US'));
+
+
+// -------------------------------------maths ----------------------------------------------------
+
+console.log(Math.PI)
+
+console.log(Math.abs(6-8));
+
+console.log(Math.round(3.6));
+
+console.log(Math.floor(3.6));
+
+console.log(Math.ceil(3.2));
+
+console.log(Math.sqrt(16));
+
+console.log(Math.pow(2, 4));
+
+console.log(Math.min(3, 5, 1, 8));
+
+console.log(Math.max(3, 5, 1, 8));
+
+
