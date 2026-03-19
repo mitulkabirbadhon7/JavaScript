f51a98496d470b2552dd9aebf40b04ed62ae1b8b
MD Mitul Kabir Badhon <126774549+mitulkabirbadhon7@users.noreply.github.com>
2026-03-20 04:07:32 +0600

    Enhance comments for random number generation
    Added comments to clarify the purpose of random number generation.

--- 01_basics/numbers&maths.js
+++ 01_basics/numbers&maths.js
@@ -38,10 +38,10 @@ const hundreds = 1000000
 // console.log(Math.max(3, 5, 1, 8));
 
 
-console.log(Math.random());
+console.log(Math.random()); // gives random value from 0 to 1 
 
-console.log((Math.random() * 10) + 1); // random number between 1 and 10
+console.log((Math.random() * 10) + 1); // random number between 1 and 10 shifting by multiply with 10
 
 const min = 10 
 const max = 20
-console.log(Math.floor(Math.random() * (max - min + 1) )+min);
+console.log(Math.floor(Math.random() * (max - min + 1) )+min); // multiply with max - min gives the range .. and + min mean that i need minimum of min value and + 1 for avoiding 0 
