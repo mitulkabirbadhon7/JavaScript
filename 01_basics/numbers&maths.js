185c461c729cb003c41197cf5d51495430fa0e3a
MD Mitul Kabir Badhon <126774549+mitulkabirbadhon7@users.noreply.github.com>
2026-03-19 22:05:42 +0000

    math and numbers

--- 01_basics/numbers&maths.js
+++ 01_basics/numbers&maths.js
@@ -19,22 +19,29 @@ const hundreds = 1000000
 
 // -------------------------------------maths ----------------------------------------------------
 
-console.log(Math.PI)
+// console.log(Math.PI)
 
-console.log(Math.abs(6-8));
+// console.log(Math.abs(6-8));
 
-console.log(Math.round(3.6));
+// console.log(Math.round(3.6));
 
-console.log(Math.floor(3.6));
+// console.log(Math.floor(3.6));
 
-console.log(Math.ceil(3.2));
+// console.log(Math.ceil(3.2));
 
-console.log(Math.sqrt(16));
+// console.log(Math.sqrt(16));
 
-console.log(Math.pow(2, 4));
+// console.log(Math.pow(2, 4));
 
-console.log(Math.min(3, 5, 1, 8));
+// console.log(Math.min(3, 5, 1, 8));
 
-console.log(Math.max(3, 5, 1, 8));
+// console.log(Math.max(3, 5, 1, 8));
 
 
+console.log(Math.random());
+
+console.log((Math.random() * 10) + 1); // random number between 1 and 10
+
+const min = 10 
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) )+min);
