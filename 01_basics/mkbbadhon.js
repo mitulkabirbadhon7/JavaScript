c12421104b0817804d6f07753245b682db99c1b3
Badhon <mitulkabirbadhon7@gmail.com>
2026-03-30 01:07:30 +0600

    update

--- 01_basics/mkbbadhon.js
+++ 01_basics/mkbbadhon.js
@@ -2,3 +2,13 @@ let name = "Badhon";
 
 console.log(name);
 
+const arr = [1,2,3,4,5,6];
+
+arr.push("Badhon");
+arr.push("(");
+arr.push(")");
+
+for(let i = 0 ; i < arr.length;i++){
+
+    console.log(arr[i]);
+}
